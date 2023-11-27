2d59e25cd18fa3d7d65c134683c21cf53d42bd1e
henrik392 <44232546+henrik392@users.noreply.github.com>
2023-11-27 15:07:32 +0100

    Test objects in main (temp)

--- src/main/java/edu/ntnu/stud/Main.java
+++ src/main/java/edu/ntnu/stud/Main.java
@@ -11,6 +11,25 @@ import java.time.Duration;
  */
 public class Main {
   public static void main(String[] args) {
-    
+    System.err.println("\n\n\n");
+
+    TrainDeparture trainDeparture = new TrainDeparture(LocalTime.of(12, 15), "R2", 1234, "Trondheim", 3,
+        Duration.ofMinutes(0));
+
+    trainDeparture.addDelay(5);
+
+    TrainRegister trainRegister = new TrainRegister();
+    trainRegister.addTrainDeparture(trainDeparture);
+    trainRegister.addTrainDeparture(new TrainDeparture(LocalTime.of(14, 30), "L2", 456, "Oslo", 2,
+        Duration.ofMinutes(10)));
+
+    System.out.println(trainRegister.getTrainDepatureFromTrainNumber(456));
+    System.out.println(trainRegister.getTrainDeparturesToDestination("Trondheim"));
+
+    System.out.println(trainRegister.sortedByTime());
+
+    System.err.println("\n\n");
+
+    System.out.println(trainRegister.deleteDeparturesBeforeTime(LocalTime.of(13, 0)));
   }
 }
