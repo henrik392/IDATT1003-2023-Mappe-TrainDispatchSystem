b1440df8b8cf602752c577479d8620c7bc2738a9
henrik392 <44232546+henrik392@users.noreply.github.com>
2023-11-28 14:12:57 +0100

    Ready for UI

--- src/main/java/edu/ntnu/stud/Main.java
+++ src/main/java/edu/ntnu/stud/Main.java
@@ -1,35 +1,17 @@
 package edu.ntnu.stud;
 
-import edu.ntnu.stud.model.TrainDeparture;
-import edu.ntnu.stud.model.TrainRegister;
-
-import java.time.LocalTime;
-import java.time.Duration;
+import edu.ntnu.stud.view.UserInterface;
 
 /**
  * The Main class contains the main method that starts the program.
  */
 public class Main {
   public static void main(String[] args) {
-    System.err.println("\n\n\n");
+    UserInterface ui = new UserInterface();
+    // Initialize dummy data
+    ui.init();
 
-    TrainDeparture trainDeparture = new TrainDeparture(LocalTime.of(12, 15), "R2", 1234, "Trondheim", 3,
-        Duration.ofMinutes(0));
-
-    trainDeparture.addDelay(5);
-
-    TrainRegister trainRegister = new TrainRegister();
-    trainRegister.addTrainDeparture(trainDeparture);
-    trainRegister.addTrainDeparture(new TrainDeparture(LocalTime.of(14, 30), "L2", 456, "Oslo", 2,
-        Duration.ofMinutes(10)));
-
-    System.out.println(trainRegister.getTrainDepatureFromTrainNumber(456));
-    System.out.println(trainRegister.getTrainDeparturesToDestination("Trondheim"));
-
-    System.out.println(trainRegister.sortedByTime());
-
-    System.err.println("\n\n");
-
-    System.out.println(trainRegister.deleteDeparturesBeforeTime(LocalTime.of(13, 0)));
+    // Start the program
+    ui.start();
   }
 }
