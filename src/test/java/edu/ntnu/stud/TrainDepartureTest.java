36038e0113c5c090df2e0430daae194820056eb2
henrik392 <44232546+henrik392@users.noreply.github.com>
2023-12-08 12:52:12 +0100

    Added docs and refactored

--- src/test/java/edu/ntnu/stud/TrainDepartureTest.java
+++ src/test/java/edu/ntnu/stud/TrainDepartureTest.java
@@ -1,45 +1,52 @@
 package edu.ntnu.stud;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import edu.ntnu.stud.model.TrainDeparture;
-
 import java.time.Duration;
 import java.time.LocalTime;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
+/** This class contains unit tests for the TrainDeparture class. */
 public class TrainDepartureTest {
   TrainDeparture trainDeparture;
 
   @BeforeEach
   public void setUp() {
-    trainDeparture = new TrainDeparture(LocalTime.of(13, 45), "L1", 123, "Trondheim", 1,
-        Duration.ofMinutes(5));
+    trainDeparture =
+        new TrainDeparture(LocalTime.of(13, 45), "L1", 123, "Trondheim", 1, Duration.ofMinutes(5));
   }
 
+  /** This nested class contains negative tests for the TrainDeparture class. */
   @Nested
   @DisplayName("Negative tests")
   public class MethodThrowsException {
     @Test
     @DisplayName("Add negative delay throws IllegalArgumentException")
     public void testAddNegativeDelay() {
-      assertThrows(IllegalArgumentException.class, () -> trainDeparture.addDelay(-10), "Delay cannot be negative");
+      assertThrows(
+          IllegalArgumentException.class,
+          () -> trainDeparture.addDelay(-10),
+          "Delay cannot be negative");
     }
 
     @Test
     @DisplayName("Constructor should throw IllegalArgumentException from negative delay")
     public void testInitWithNegativeDelay() {
-      assertThrows(IllegalArgumentException.class,
-          () -> new TrainDeparture(LocalTime.of(13, 45), "L1", 123, "Trondheim", 1, Duration.ofMinutes(-5)),
+      assertThrows(
+          IllegalArgumentException.class,
+          () ->
+              new TrainDeparture(
+                  LocalTime.of(13, 45), "L1", 123, "Trondheim", 1, Duration.ofMinutes(-5)),
           "Delay cannot be negative");
     }
   }
 
+  /** This nested class contains positive tests for the TrainDeparture class. */
   @Nested
   @DisplayName("Positive tests")
   public class MethodDoesNotThrowException {
@@ -54,7 +61,7 @@ public class TrainDepartureTest {
     @DisplayName("Delay gets added to departureTime on calling getDelayedTime")
     public void testGetDelayedTime() {
       trainDeparture.addDelay(5);
-      assertEquals(LocalTime.of(13, 55), trainDeparture.getDelayedTime());
+      assertEquals(LocalTime.of(13, 55), trainDeparture.getDepartureTimeWithDelay());
     }
 
     @Test
